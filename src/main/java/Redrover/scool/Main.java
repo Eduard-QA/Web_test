1ccbe1175a22247ca92c2ea705f3b859e7101fc0
Eduard-QA <eduard.mickel@gmail.com>
2024-10-12 16:17:00 +0300

    webDrivers

--- src/main/java/Redrover/scool/Main.java
+++ src/main/java/Redrover/scool/Main.java
@@ -2,11 +2,21 @@ package Redrover.scool;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://mvnrepository.com");
+        String title = driver.getTitle();
+        System.out.println("Title page is: " + title);
+        driver.quit();
+
+        WebDriver fireFox = new FirefoxDriver();
+        fireFox.get("https://www.google.co.uk/");
+        String title1 = fireFox.getTitle();
+        System.out.println("Title page is: " + title1);
+        fireFox.quit();
 
     }
 }
--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,16 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class WebTest {
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeMethod
+    public void baseURL() {
+        driver.get("https://www.google.co.uk/");
+        driver.manage().window().fullscreen();
+
+    }
+
     @Test
     public void webTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.co.uk/");
 
-        WebDriver fireFox = new FirefoxDriver();
-        fireFox.get("https://www.google.co.uk/");
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.findElement(By.name("q")).sendKeys("duck");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+
+
+    }
+
+    @AfterMethod
+    public void closeWindow(){
+        driver.quit();
     }
 }
