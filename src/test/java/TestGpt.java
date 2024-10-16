182697e8a1a9ca4a9333c47a16325b0a2c29ce7d
Eduard-QA <eduard.mickel@gmail.com>
2024-10-16 20:46:32 +0300

    cosmetic changes

--- src/test/java/TestGpt.java
+++ src/test/java/TestGpt.java
@@ -9,38 +9,38 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestGpt {
-private WebDriver driver;
-private ChromeOptions options;
-
-
+    private WebDriver driver;
+    private ChromeOptions options;
 
     @BeforeMethod
-    public void enter(){
+    public void enter() {
         options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         driver = new ChromeDriver(options);
         driver.get("https://chatgpt.com/");
 
     }
+
     @AfterMethod
-    public void quit(){
-        if(driver != null){
+    public void quit() {
+        if (driver != null) {
             driver.quit();
         }
     }
+
     @Test
-    public void getTitle(){
+    public void getTitle() {
         System.out.println(driver.getTitle());
         Assert.assertEquals(driver.getTitle(), "ChatGPT");
 
     }
+
     @Test
-    public void checkDialogWindow(){
+    public void checkDialogWindow() {
         WebElement newChatButton = driver.findElement(By.xpath("//span[@class='flex']"));
         newChatButton.click();
 
         WebElement dialogWindow = driver.findElement(By.xpath("//div[@role='dialog']"));
-        System.out.println(dialogWindow.getText());
         Assert.assertTrue(dialogWindow.getText().contains("Clear current chat"));
 
     }
